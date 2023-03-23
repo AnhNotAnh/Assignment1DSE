b0d00e39e54c0bb4070653bc50415e982f3d74a3
Duy Quoc Anh Nguyen <ngudy105@mymail.unisa.edu.au>
2023-03-23 10:36:51 +1030

    First touch Field

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,10 +1,20 @@
 package students;
-
+import src.students.items.Soil;
 
 public class Field {
 	
+	protected String[][] field;
+	
 	public Field(int height, int width)
 	{
+		this.field = new String[width][height];
+		for (int witdthcount = 0; witdthcount < width; witdthcount++)
+		{
+			for (int heightcount = 0; heightcount < height; heightcount++)
+			{
+				Soil soil = new Soil(); //how can field create new soil to put it inside array?
+				field [witdthcount][heightcount]= soil.toString();
+			}
+		}
 	}
-	
 }
