d9e2e8bd73ca833eada0f2bfa7da5c9850c177f6
Duy Quoc Anh Nguyen <ngudy105@mymail.unisa.edu.au>
2023-04-09 12:37:21 +0930

    Add try catch in run()
    This will handle StringIndexOutOfBoundsException which is when user have an empty input and program substring it.
    Move all if else condition inside try statement.

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -34,86 +34,86 @@ public class Farm {
 								"  q: quit" +"\n");
 			Scanner keyboard = new Scanner(System.in);
 			String userInput = keyboard.nextLine();
-			System.out.println(userInput); // delete later
 			userInput = userInput.trim();
-			System.out.println(userInput); // delete later
-			if(userInput.substring(0,1).equals("q"))
+			try 
 			{
-				quit = false;
-			}
-			else if(userInput.substring(0,1).equals("t")) // e.g t 10 2 -> width = 10 and height =2 
-			{
-				int firstSpaceIdx = userInput.indexOf(" "); // return 1
-				String location = userInput.substring(firstSpaceIdx + 1); // return "10 2"
-				int secondSpaceIdx = location.indexOf(" "); // return 2
-				
-				int width = Integer.parseInt(location.substring(0, secondSpaceIdx)); // return 10
-				int height = Integer.parseInt(location.substring(secondSpaceIdx + 1)); // return 2
-				fieldObj.till(height -1, width -1); //(x, y)
-				
-				System.out.println(firstSpaceIdx+ " "+location+" " + secondSpaceIdx+ "\n"); // return 1 10 2 2
-				System.out.println(width +" "+ height ); // 10 2
-			}
-			else if(userInput.substring(0,1).equals("h"))
-			{
-				int width = Integer.parseInt(userInput.substring(2,3));
-				int height = Integer.parseInt(userInput.substring(4));
-				String foodName = fieldObj.field[height-1][width-1].toString();
-				int value = fieldObj.harvest(height -1, width -1);
-				startingFunds += value;
-				System.out.println("Sold "+ foodName + " for "
-									+ value+"\n");
-			}
-			else if(userInput.substring(0,1).equals("p")) 
-			{
-				int width = Integer.parseInt(userInput.substring(2,3));
-				int height = Integer.parseInt(userInput.substring(4));
-				if(fieldObj.field[height-1][width-1] instanceof Food)
+				if(userInput.substring(0,1).equals("q"))
 				{
-					System.out.println("Food has been planted");
+					quit = false;
 				}
-				else 
+				else if(userInput.substring(0,1).equals("t")) // e.g t 10 2 -> width = 10 and height =2 , fix other functions !!!!!!!!
 				{
-					System.out.println("Enter:"+"\n"+
-										"- 'a' to buy an apple for $2" + "\n"+
-										"- 'g' to buy an grain for $1"+ "\n");
-					String plantTree = keyboard.nextLine();
-					if(plantTree.substring(0).equals("a") && startingFunds - Apples.getCost() >=0 )
+					int firstSpaceIdx = userInput.indexOf(" "); // return 1
+					String location = userInput.substring(firstSpaceIdx + 1); // return "10 2"
+					int secondSpaceIdx = location.indexOf(" "); // return 2
+					
+					int width = Integer.parseInt(location.substring(0, secondSpaceIdx)); // return 10
+					int height = Integer.parseInt(location.substring(secondSpaceIdx + 1)); // return 2
+					fieldObj.till(height -1, width -1); //(x, y)
+					
+					System.out.println(firstSpaceIdx+ " "+location+" " + secondSpaceIdx+ "\n"); // return 1 10 2 2
+					System.out.println(width +" "+ height ); // 10 2
+				}
+				else if(userInput.substring(0,1).equals("h"))
+				{
+					int width = Integer.parseInt(userInput.substring(2,3));
+					int height = Integer.parseInt(userInput.substring(4));
+					String foodName = fieldObj.field[height-1][width-1].toString();
+					int value = fieldObj.harvest(height -1, width -1);
+					startingFunds += value;
+					System.out.println("Sold "+ foodName + " for "
+										+ value+"\n");
+				}
+				else if(userInput.substring(0,1).equals("p")) 
+				{
+					int width = Integer.parseInt(userInput.substring(2,3));
+					int height = Integer.parseInt(userInput.substring(4));
+					if(fieldObj.field[height-1][width-1] instanceof Food)
 					{
-						fieldObj.plant(height-1, width-1, new Apples());
-						startingFunds -= Apples.getCost();
+						System.out.println("Food has been planted");
 					}
-					else if(plantTree.substring(0).equals("g") && startingFunds - Grain.getCost() >=0 )
+					else 
 					{
-						fieldObj.plant(height-1, width -1, new Grain());
-						startingFunds -= Grain.getCost();
-					}
-					else
-					{
-						System.out.println("Invalid input");
+						System.out.println("Enter:"+"\n"+
+											"- 'a' to buy an apple for $2" + "\n"+
+											"- 'g' to buy an grain for $1"+ "\n");
+						String plantTree = keyboard.nextLine();
+						if(plantTree.substring(0).equals("a") && startingFunds - Apples.getCost() >=0 )
+						{
+							fieldObj.plant(height-1, width-1, new Apples());
+							startingFunds -= Apples.getCost();
+						}
+						else if(plantTree.substring(0).equals("g") && startingFunds - Grain.getCost() >=0 )
+						{
+							fieldObj.plant(height-1, width -1, new Grain());
+							startingFunds -= Grain.getCost();
+						}
+						else
+						{
+							System.out.println("Invalid input");
+						}
 					}
 				}
+				else if(userInput.substring(0,1).equals("s"))
+				{
+					System.out.println(fieldObj.getSummary());
+				}
+				else if(userInput.substring(0,1).equals("w"))
+				{
+					fieldObj.tick();
+					continue;
+				}
+				else
+				{
+					System.out.println("Invalid input, please try again !"+"\n");
+					continue;
+				}
+				fieldObj.tick(); // after each action, the field ages.
 			}
-			else if(userInput.substring(0,1).equals("s"))
+			catch(StringIndexOutOfBoundsException e)
 			{
-				System.out.println(fieldObj.getSummary());
+				System.out.println("Empty input found, please try again !"+"\n");
 			}
-			else if(userInput.substring(0,1).equals("w"))
-			{
-				fieldObj.tick();
-				continue;
-			}
-//			else if(userInput.substring(0,1).equals("")) // how handle StringIndexOutOfBoundsException ??
-//			{
-//				System.out.println("Empty input found, please try again !"+"\n");
-//				continue;
-//			}
-			else
-			{
-				System.out.println("Invalid input, please try again !"+"\n");
-				continue;
-			}
-			fieldObj.tick(); // after each action, the field ages.
 		}
 	}
 }
