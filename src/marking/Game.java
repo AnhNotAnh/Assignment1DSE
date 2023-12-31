269a00a3a54e26e7a628ebdae7b3aeb3e36c759d
Duy Quoc Anh Nguyen <84012972+AnhNotAnh@users.noreply.github.com>
2023-12-31 22:26:11 +1030

    Seperate run game and testing into 2 files

---
+++ src/marking/Game.java
@@ -0,0 +1,12 @@
+package marking;
+import students.Farm;
+
+public class Game {
+	public static void main(String[] args) throws CloneNotSupportedException {
+		
+		Farm f = new Farm(10, 5, 10);
+		f.run();
+		
+	}
+
+}
--- src/marking/Marker.java
+++ src/marking/Marker.java
@@ -1,6 +1,5 @@
 package marking;
 
-import students.Farm;
 import students.Field;
 import students.items.*;
 
@@ -46,13 +45,6 @@ public class Marker {
 		testField.till(0, 0);
 		System.out.println(a.equals(testField.get(0, 0))); //should print false
 		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
-		
-
-		// Section THREE
-		// uncomment this section once you get to it
-		//remember, your farm should work on varies size fields (assuming all fields are less than 10x10)
-		Farm f = new Farm(10, 5, 10);
-		f.run();
 		
 	}
 
